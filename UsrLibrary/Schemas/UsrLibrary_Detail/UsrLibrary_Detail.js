c029b9eab24148722b3048262811299afc84aaed
Behruz <6kbet@mail.ru>
2024-06-11 16:04:53 +0500

    Added the Library application

---
+++ UsrLibrary/Schemas/UsrLibrary_Detail/UsrLibrary_Detail.js
@@ -0,0 +1,8 @@
+define('UsrLibrary_Detail', [], function () {
+	return {
+		entitySchemaName: 'UsrLibrary',
+		details: /**SCHEMA_DETAILS*/ {} /**SCHEMA_DETAILS*/,
+		diff: /**SCHEMA_DIFF*/ [] /**SCHEMA_DIFF*/,
+		methods: {},
+	};
+});
