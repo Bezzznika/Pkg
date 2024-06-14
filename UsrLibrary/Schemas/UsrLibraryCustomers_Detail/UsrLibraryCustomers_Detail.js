184faae93afdc671d6ed67ec66a3f01229305f99
Behruz <6kbet@mail.ru>
2024-06-14 11:48:46 +0500

    Added user and order sections to library application

---
+++ UsrLibrary/Schemas/UsrLibraryCustomers_Detail/UsrLibraryCustomers_Detail.js
@@ -0,0 +1,8 @@
+define('UsrLibraryCustomers_Detail', [], function () {
+	return {
+		entitySchemaName: 'UsrLibraryCustomers',
+		details: /**SCHEMA_DETAILS*/ {} /**SCHEMA_DETAILS*/,
+		diff: /**SCHEMA_DIFF*/ [] /**SCHEMA_DIFF*/,
+		methods: {},
+	};
+});
---
+++ UsrLibrary/Schemas/UsrLibraryOrders_Detail/UsrLibraryOrders_Detail.js
@@ -0,0 +1,8 @@
+define('UsrLibraryOrders_Detail', [], function () {
+	return {
+		entitySchemaName: 'UsrLibraryOrders',
+		details: /**SCHEMA_DETAILS*/ {} /**SCHEMA_DETAILS*/,
+		diff: /**SCHEMA_DIFF*/ [] /**SCHEMA_DIFF*/,
+		methods: {},
+	};
+});
